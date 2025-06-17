d4ed57215fb16f6f738a3e0eb72931126152df4e
kubus765 <gdx161267@student.gdansk.merito.pl>
2025-06-17 10:14:04 +0200

    Revert "add 2FA"
    This reverts commit 5e99f187e6c5492d891d7b7371aacba012f3adf3.

--- crm/middleware/__init__.py
+++
@@ -1,9 +0,0 @@
-from .viewer_restrict import ViewerRestrictMiddleware
-from .email_verification import EmailVerificationMiddleware
-from .two_factor import TwoFactorMiddleware
-
-__all__ = [
-    'ViewerRestrictMiddleware',
-    'EmailVerificationMiddleware',
-    'TwoFactorMiddleware',
-]
--- projekt_wdrozeniowy/urls.py
+++ projekt_wdrozeniowy/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('crm.urls')),
-    path('two-factor/', include('two_factor.urls')),
 ]
 
 if settings.DEBUG:
