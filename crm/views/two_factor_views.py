e16f93533c0de55826de6d89e5e2d2517d16fe1a
kubus765 <gdx161267@student.gdansk.merito.pl>
2025-07-31 15:15:05 +0200

    Update two_factor_views.py

--- crm/views/two_factor_views.py
+++ crm/views/two_factor_views.py
@@ -312,8 +312,8 @@ def generate_qr_code(data):
 
 def get_client_ip(request):
     """Get client IP address from request"""
-
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')    if x_forwarded_for:
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
