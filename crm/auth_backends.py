9f187095b02d3d98c27629ddab9b0a3a7f4fdaa8
kubus765 <gdx161267@student.gdansk.merito.pl>
2025-08-06 13:56:27 +0200

    Update auth_backends.py

--- crm/auth_backends.py
+++ crm/auth_backends.py
@@ -1,6 +1,3 @@
-from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
-from .models import ActivityLog
 import logging
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
@@ -9,24 +6,6 @@ from django.db.models import Q
 logger = logging.getLogger(__name__)
 
 User = get_user_model()
-
-@receiver(user_logged_in)
-def log_user_login(sender, request, user, **kwargs):
-    """Log user login using Django's built-in signal"""
-    # Get client IP
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    
-    # Create activity log entry
-    ActivityLog.objects.create(
-        user=user,
-        action_type='login',
-        description=f"User {user.username} logged in",
-        ip_address=ip
-    )
 
 class EmailOrUsernameModelBackend(ModelBackend):
     """
