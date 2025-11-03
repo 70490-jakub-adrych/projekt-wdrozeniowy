acfe51f12c1125168c8d84bf768c3fad6fc6d543
kubus765 <gdx161267@student.gdansk.merito.pl>
2025-11-03 20:10:12 +0100

    Update 0006_alter_ticketcalendarassignment_unique_together.py

--- crm/migrations/0006_alter_ticketcalendarassignment_unique_together.py
+++ crm/migrations/0006_alter_ticketcalendarassignment_unique_together.py
@@ -3,6 +3,42 @@
 from django.db import migrations
 
 
+def drop_old_indexes(apps, schema_editor):
+    """Drop problematic indexes manually before altering unique_together"""
+    if schema_editor.connection.vendor == 'mysql':
+        with schema_editor.connection.cursor() as cursor:
+            # Drop the old unique constraint
+            try:
+                cursor.execute(
+                    "ALTER TABLE crm_ticketcalendarassignment "
+                    "DROP INDEX crm_ticketcalendarassign_ticket_id_assigned_to_id_7f445d8d_uniq"
+                )
+            except Exception:
+                pass  # Index might not exist
+            
+            # Drop any duplicate indexes that might exist
+            try:
+                cursor.execute(
+                    "ALTER TABLE crm_ticketcalendarassignment "
+                    "DROP INDEX crm_ticketcalendarassignment_ticket_id_e1ad8f37"
+                )
+            except Exception:
+                pass  # Index might not exist
+            
+            try:
+                cursor.execute(
+                    "ALTER TABLE crm_ticketcalendarassignment "
+                    "DROP INDEX crm_ticketcalendarassignment_assigned_to_id_6c8b0e5f"
+                )
+            except Exception:
+                pass  # Index might not exist
+
+
+def reverse_indexes(apps, schema_editor):
+    """Recreate original indexes if needed"""
+    pass  # We'll let Django handle the reverse
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -10,11 +46,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # First, remove the old unique_together constraint
-        migrations.AlterUniqueTogether(
-            name='ticketcalendarassignment',
-            unique_together=set(),
-        ),
+        # First, manually drop old indexes
+        migrations.RunPython(drop_old_indexes, reverse_indexes),
         # Then, add the new unique_together constraint
         migrations.AlterUniqueTogether(
             name='ticketcalendarassignment',
